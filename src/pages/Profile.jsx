4918c704c6df70e8ff9728e29706f7b54dea8c01
Leonardo Herrera <leonardo.herrera.13498804@usalesiana.edu.bo>
2023-04-29 18:31:06 -0400

    deletions & basicNavigation

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import BottomNav from '../partials/BottomNav';
+import Header from '../partials/Header';
+
+export default function Profile() {
+  return (
+    <div className='w-full'>
+      <Header />
+      <div>Profile</div>
+      <BottomNav/>
+    </div>
+  );
+}
---
+++ src/pages/Truck.jsx
@@ -0,0 +1,12 @@
+import BottomNav from '../partials/BottomNav';
+import Header from '../partials/Header';
+
+export default function Trucks() {
+  return (
+    <div className='w-full'>
+      <Header />
+      <div>GARBAGE TRUCKS</div>
+      <BottomNav/>
+    </div>
+  );
+}
